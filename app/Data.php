78b2e400bca77984835ffbfc9f017bf5ca047604
skarampatakis <s.karampatakis@gmail.com>
2018-07-11 15:05:05 +0300

    fixed data endpoint

---
+++ app/Data.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App;
+
+use Illuminate\Http\Request;
+use Asparagus\QueryBuilder;
+use Cache;
+
+class Data
+{
+    use BrowserTrait;
+    
+    function __construct($request, $resource, $internal = false) {
+        $this->request = $request;
+        $this->resource = $resource; 
+        $this->internal = $internal;
+        $this->uri = $this->setUri();
+        $this->endpoint = \App\Endpoint::first()->endpoint_url;
+        $this->directQuery = $this->getDirectQuery();
+        $this->reverseQuery = $this->getReverseQuery();
+        $this->bnodeQuery = $this->getBnodeQuery();
+        $this->graph = $this->setGraph();
+    }
+    
+    private static $extensions = [
+        "rdf",
+        "nt",
+        "csv",
+        "ttl",
+        "n3",
+        "json",
+        "xml",
+        "html"
+    ];
+    
+    private function setUri(){
+        if (!$this->internal) {
+            $path_parts = $this->mb_pathinfo($this->resource);            
+            if (isset($path_parts['extension']) && in_array($path_parts['extension'], self::$extensions)) {
+                $this->extension = $path_parts['extension'];
+                if ($path_parts['dirname'] != '.') {
+                    $this->resource = $path_parts['dirname'] . '/' . $path_parts['filename'];
+                } else {
+                    $this->resource = $path_parts['filename'];
+                }
+            }
+            return $this->constructIRI2($this->request, $this->resource);
+        } else {
+            return $this->resource;
+        }
+    }
+    
+    private function getDirectQuery(){
+        
+        
+        $sparql = new \EasyRdf_Sparql_Client($this->endpoint);
+        $query = new QueryBuilder();
+        $query->select("?p", "?o")
+                ->where("<" . $this->uri . ">", "?p", "?o");
+        $result = $sparql->query($query);
+        return $result;
+    }
+    
+    private function getReverseQuery(){
+        $sparql = new \EasyRdf_Sparql_Client($this->endpoint);
+        $query = new QueryBuilder();
+        $query->select("?s", "?p")
+                ->where("?s", "?p", "<" . $this->uri . ">");
+        $result = $sparql->query($query);
+        return $result;        
+    }
+    
+    private function getBnodeQuery(){
+        $sparql = new \EasyRdf_Sparql_Client($this->endpoint);
+        $query = new QueryBuilder();
+        $query->select("?bnode", "?p2", "?value")
+                ->where("<" . $this->uri . ">", "?p", "?o")
+                ->also("?bnode", "?p2", "?value")
+                ->filter("isBlank(?bnode)");
+        $result = $sparql->query($query);
+        return $result;
+        
+    }
+    
+    public function setGraph(){
+        $graph = new \EasyRdf_Graph;
+        foreach ($this->directQuery as $triple) {
+            $graph->add(new \EasyRdf_Resource($this->uri), $triple->p, $triple->o);
+        }        
+        
+        foreach ($this->reverseQuery as $triple) {
+            $graph->add($triple->s, $triple->p, new \EasyRdf_Resource($this->uri));
+        }
+        foreach ($this->bnodeQuery as $triple) {
+            $graph->add($triple->bnode, $triple->p2, $triple->value);
+        }
+
+        Cache::put($this->uri, $graph, 100);
+        
+        return $graph;        
+    }
+    
+    public function getMIME() {
+        switch ($this->extension) {
+            case 'rdf':
+                $MIME = 'application/rdf+xml';
+                break;
+            case 'n3' :
+                $MIME = 'text/n3';
+                break;
+            case 'nt' :
+                $MIME = 'application/n-triples';
+                break;
+            case 'jsonld':
+                $MIME = 'application/ld+json';
+                break;
+            case 'ttl':
+                $MIME = 'text/turtle';
+                break;
+            case 'csv':
+                $MIME = 'text/csv';
+                break;
+            case 'json':
+                $MIME = 'application/rdf+json';
+                break;
+            default:
+                $MIME = 'application/rdf+xml';
+        }
+        return $MIME;
+    }
+    
+    public function getContentType(Request $request) {
+        //instantiate negotiator
+        $negotiator = new \Negotiation\Negotiator();
+
+        //get the header
+        $header = $request->header('Accept');
+
+        //set the available content types
+        $priorities = array('application/rdf+xml', 'text/n3', 'application/ld+json', 'text/turtle', 'application/rdf+json', 'application/n-triples');
+
+        //get the best match
+        $mediaType = $negotiator->getBest($header, $priorities);
+        //dd($mediaType);
+        //get the value
+        if ($mediaType != null) {
+            $type = $mediaType->getValue();
+        } else {
+            $type = 'application/rdf+xml';
+        }
+
+        return $type;
+    }
+    
+    public function setFormat(){
+        if (isset($this->extension)) {
+
+            $MIME = $this->getMime($this->extension);
+        } else {
+            $MIME = $this->getContentType($this->request);
+        }
+        $this->MIME = $MIME;
+        $this->format = \EasyRdf_Format::getFormat($MIME);
+    }
+    
+    public function download() {
+        
+        $this->setFormat();
+        //serialize the graph        
+        $content = $this->graph->serialise($this->format);
+        
+        $length = strlen($content);
+
+        $extension = \EasyRdf_Format::getFormat($this->MIME)->getDefaultExtension();
+
+        http_response_code(200);
+
+        header('Connection: Keep-Alive');
+
+        header('Content-Description: File Transfer');
+
+        header('Content-Disposition: inline; filename=' . $this->resource . '.' . $extension);
+
+        header('Accept-Ranges: bytes');
+
+        header('Content-Type: ' . $this->MIME);
+
+        header('Content-Length: ' . $length);
+
+        echo $content;
+
+        exit;
+    }
+
+}
--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -2,164 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Cache;
+use App\Data;
+use Illuminate\Http\Request;
+
+
 
 class DataController extends Controller {
 
     use \App\BrowserTrait;
 
-    private static $extensions = [
-        "rdf",
-        "nt",
-        "csv",
-        "ttl",
-        "n3",
-        "json",
-        "xml",
-        "html"
-    ];
-
     public function data(Request $request, $resource, $internal = false) {
 
-        $this->setNamespaces();
-
-        $endpoint = \App\Endpoint::first();
-        $sparql = new \EasyRdf_Sparql_Client($endpoint->endpoint_url);
-        if (!$internal) {
-            $path_parts = $this->mb_pathinfo($resource);            
-            if (isset($path_parts['extension']) && in_array($path_parts['extension'], self::$extensions)) {
-                $extension = $path_parts['extension'];
-                if ($path_parts['dirname'] != '.') {
-                    $resource = $path_parts['dirname'] . '/' . $path_parts['filename'];
-                } else {
-                    $resource = $path_parts['filename'];
-                }
-            }
-            $uri = $this->constructIRI2($request, $resource);
-        } else {
-            $uri = $resource;
-        }
-
-
-        //create queries
-
-        $direct_query = 'SELECT ?p ?o WHERE {<' . $uri . '> ?p ?o . }';
-        $direct_result = $sparql->query($direct_query);
-
-        $reverse_query = 'select ?s ?p where {?s ?p <' . $uri . '> } ';
-        $reverse_result = $sparql->query($reverse_query);
-        $bnode_query = 'select ?bnode ?p2 ?value where {<' . $uri . '> ?p ?bnode . ?bnode ?p2 ?value .  filter isBlank(?bnode)} ';
-        $bnode_result = $sparql->query($bnode_query);
-
-        $graph = new \EasyRdf_Graph;
-        foreach ($direct_result as $triple) {
-            $graph->add(new \EasyRdf_Resource($uri), $triple->p, $triple->o);
-        }
-        foreach ($reverse_result as $triple) {
-            $graph->add($triple->s, $triple->p, new \EasyRdf_Resource($uri));
-        }
-        foreach ($bnode_result as $triple) {
-            $graph->add($triple->bnode, $triple->p2, $triple->value);
-        }
-        
-        if ($internal) {
-            Cache::put($uri, $graph, 100);
-            return $graph;
-        }
-        //get the format
-        if (isset($extension)) {
-
-            $MIME = DataController::getMime($extension);
-        } else {
-            $MIME = DataController::getContentType($request);
-        }
-
-        $format = \EasyRdf_Format::getFormat($MIME);
+        $model = new Data($request, $resource, $internal);
+                
+        if($model->internal){
+            return $model->graph;
+        }        
         //set status
-        $status = $graph->isEmpty() ? 404 : 200;
-        //serialize the graph
-        $content = $graph->serialise($format);
+        if($model->graph->isEmpty()){
+            return response("Resource not found", 404);
+        }        
         //create and send the file
-        DataController::createFile($content, $MIME, $resource, $status);
-        exit;
+        $model->download();        
     }
-
-    public function getMIME($extension) {
-        switch ($extension) {
-            case 'rdf':
-                $MIME = 'application/rdf+xml';
-                break;
-            case 'n3' :
-                $MIME = 'text/n3';
-                break;
-            case 'nt' :
-                $MIME = 'application/n-triples';
-                break;
-            case 'jsonld':
-                $MIME = 'application/ld+json';
-                break;
-            case 'ttl':
-                $MIME = 'text/turtle';
-                break;
-            case 'csv':
-                $MIME = 'text/csv';
-                break;
-            case 'json':
-                $MIME = 'application/rdf+json';
-                break;
-            default:
-                $MIME = 'application/rdf+xml';
-        }
-        return $MIME;
-    }
-
-    public function getContentType(Request $request) {
-        //instantiate negotiator
-        $negotiator = new \Negotiation\Negotiator();
-
-        //get the header
-        $header = $request->header('Accept');
-
-        //set the available content types
-        $priorities = array('application/rdf+xml', 'text/n3', 'application/ld+json', 'text/turtle', 'application/rdf+json', 'application/n-triples');
-
-        //get the best match
-        $mediaType = $negotiator->getBest($header, $priorities);
-        //dd($mediaType);
-        //get the value
-        if ($mediaType != null) {
-            $type = $mediaType->getValue();
-        } else {
-            $type = 'application/rdf+xml';
-        }
-
-        return $type;
-    }
-
-    public function createFile($content, $MIME, $resource, $status) {
-
-        $length = strlen($content);
-
-        $extension = \EasyRdf_Format::getFormat($MIME)->getDefaultExtension();
-
-        http_response_code($status);
-
-        header('Connection: Keep-Alive');
-
-        header('Content-Description: File Transfer');
-
-        header('Content-Disposition: inline; filename=' . $resource . '.' . $extension);
-
-        header('Accept-Ranges: bytes');
-
-        header('Content-Type: ' . $MIME);
-
-        header('Content-Length: ' . $length);
-
-        echo $content;
-
-        exit;
-    }
-
 }
